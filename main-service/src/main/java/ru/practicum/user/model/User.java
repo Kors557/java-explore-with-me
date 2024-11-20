b3775dc1f596039ab3d0b22598c0dfcf5cb4d138
Kors557 <1500459@mail.ru>
2024-11-20 18:59:58 +0300

    Ревью

--- main-service/src/main/java/ru/practicum/user/model/User.java
+++ main-service/src/main/java/ru/practicum/user/model/User.java
@@ -1,17 +1,7 @@
 package ru.practicum.user.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import jakarta.persistence.*;
+import lombok.*;
 import org.hibernate.proxy.HibernateProxy;
 
 import java.util.Objects;
